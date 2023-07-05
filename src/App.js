4c00d493da7cbc93e8a99a473a87911f9dff27b5
Prakash L <121917226+prakashlohanathan@users.noreply.github.com>
2023-07-05 13:10:15 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,77 @@
+import React, { useState } from 'react';
+import Card from './Components/Card';
+import './index.css';
+import DropDown from './Components/DropDown';
+
+const App = () => {
+  const [todos, setTodos] = useState([]);
+  const [taskInputValue, setTaskInputValue] = useState('');
+  const [taskDescriptionValue, setTaskDescriptionValue] = useState('');
+  const [statusFilter, setStatusFilter] = useState('All');
+
+  const handleTodo = () => {
+    const todo = { name: taskInputValue, description: taskDescriptionValue, status: 'Not Completed' };
+    setTodos([...todos, todo]);
+    setTaskInputValue('');
+    setTaskDescriptionValue('');
+  };
+
+  const handleDelete = (todo) => {
+    setTodos(todos.filter((item) => item !== todo));
+  };
+
+  const handleEdit = (todo, editedName, editedDescription, status) => {
+    setTodos(
+      todos.map((item) => {
+        if (item === todo) {
+          return { ...item, name: editedName, description: editedDescription, status };
+        }
+        return item;
+      })
+    );
+  };
+
+  const handleStatusFilter = (selectedStatus) => {
+    setStatusFilter(selectedStatus);
+  };
+
+  const filteredTodos = todos.filter((todo) => {
+    if (statusFilter === 'All') {
+      return true;
+    } else if (statusFilter === 'Completed') {
+      return todo.status === 'Completed';
+    } else if (statusFilter === 'Not Completed') {
+      return todo.status === 'Not Completed';
+    }
+    return false;
+  });
+
+  return (
+    <div>
+      <h1><b>MY TODO APP</b></h1>
+      <div className="task-form" style={{ marginTop: '10%' }}>
+        <input
+          value={taskInputValue}
+          onChange={(e) => setTaskInputValue(e.target.value)}
+          placeholder="Todo Name"
+        />
+        &nbsp; &nbsp; &nbsp;
+        <input
+          value={taskDescriptionValue}
+          onChange={(e) => setTaskDescriptionValue(e.target.value)}
+          placeholder="Todo Description"
+        />
+        &nbsp; &nbsp;
+        <button onClick={handleTodo}>Add Todo</button>
+      </div>
+      <DropDown selectedOption={statusFilter} onSelect={handleStatusFilter} />
+      <div className="task-list" style={{ display: 'flex', flexWrap: 'wrap' }}>
+        {filteredTodos.map((todo, index) => (
+          <Card key={`todo-item-${index}`} todo={todo} onDelete={handleDelete} onEdit={handleEdit} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/Components/DropDown.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const DropDown = ({ selectedOption, onSelect }) => {
+  const handleSelect = (e) => {
+    const selectedStatus = e.target.value;
+    if (typeof onSelect === 'function') {
+      onSelect(selectedStatus);
+    }
+  };
+
+  return (
+    <div style={{ display: 'flex', justifyContent: 'flex-end', margin: '10px' }}>
+      <label htmlFor="status-filter"><h4>Filter by Status</h4></label>
+      <select id="status-filter" value={selectedOption} onChange={handleSelect}>
+        <option value="All">All</option>
+        <option value="Completed">Completed</option>
+        <option value="Not Completed">Not Completed</option>
+      </select>
+    </div>
+  );
+};
+
+export default DropDown;
